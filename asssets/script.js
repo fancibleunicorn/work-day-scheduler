4839aa38096a96419313d5074304b931ce718188
Adam Crandall <adamcrandall91@gmail.com>
2020-11-01 13:14:11 -0500

    added pseudo code to jscript

---
+++ asssets/script.js
@@ -0,0 +1,9 @@
+//Display Current Date at Top of Calendar
+
+
+//Make Date Block's color-coded based on Past, Present, or Future
+
+
+//On time block click, enter an event into each time block
+
+//on save button click, save time block text to local storage
